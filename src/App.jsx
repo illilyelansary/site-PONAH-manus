e57e6b89e38f41c8c64181a78c36fc0f8b372132
illilyelansary <illilyelansary@gmail.com>
2025-07-10 20:10:42 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 
-// Import des pages
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Members from './components/pages/Members';
 import Activities from './components/pages/Activities';
-import News from './components/pages/News';
 import Publications from './components/pages/Publications';
+import News from './components/pages/News';
 import Contact from './components/pages/Contact';
-
-// Import éventuel de Layout/Navbar si tu en as un
-// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      {/* <Navbar /> si tu as une barre de navigation globale */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<About />} />
-        <Route path="/membres" element={<Members />} />
-        <Route path="/activites" element={<Activities />} />
-        <Route path="/actualites" element={<News />} />
-        <Route path="/publications" element={<Publications />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/apropos" element={<About />} />
+            <Route path="/membres" element={<Members />} />
+            <Route path="/activites" element={<Activities />} />
+            <Route path="/publications" element={<Publications />} />
+            <Route path="/actualites" element={<News />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 }
