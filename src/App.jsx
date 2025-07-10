4feaeb436cf82d090693c2452fa11480f5fa9f31
illilyelansary <illilyelansary@gmail.com>
2025-07-10 20:04:36 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 
-// Components
-import Header from './components/Header';
-import Footer from './components/Footer';
+// Import des pages
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Members from './components/pages/Members';
 import Activities from './components/pages/Activities';
-import Publications from './components/pages/Publications';
 import News from './components/pages/News';
+import Publications from './components/pages/Publications';
 import Contact from './components/pages/Contact';
+
+// Import éventuel de Layout/Navbar si tu en as un
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/members" element={<Members />} />
-            <Route path="/activities" element={<Activities />} />
-            <Route path="/publications" element={<Publications />} />
-            <Route path="/news" element={<News />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      {/* <Navbar /> si tu as une barre de navigation globale */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apropos" element={<About />} />
+        <Route path="/membres" element={<Members />} />
+        <Route path="/activites" element={<Activities />} />
+        <Route path="/actualites" element={<News />} />
+        <Route path="/publications" element={<Publications />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
